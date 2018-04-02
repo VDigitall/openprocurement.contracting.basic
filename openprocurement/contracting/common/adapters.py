8d36d8534bb89975f90afe6e62d9feee045354d0
annawzz <annawzz@quintagroup.org>
2018-04-02 10:08:47 +0300

    Use adapters

---
+++ openprocurement/contracting/common/adapters.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from openprocurement.contracting.core.adapters import ContractConfigurator
+
+
+class ContractCommonConfigurator(ContractConfigurator):
+    """ Common contract configuration adapter """
+
+    name = "Common configurator"
