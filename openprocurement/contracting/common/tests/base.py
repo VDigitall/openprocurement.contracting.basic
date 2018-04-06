513a350d65e56ebe917515c9a89bf2959e9c2cd5
annawzz <annawzz@quintagroup.org>
2018-04-06 14:16:22 +0300

    Add dumps base test class for docs

--- openprocurement/contracting/common/tests/base.py
+++ openprocurement/contracting/common/tests/base.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
+import json
 from copy import deepcopy
+from webtest import TestApp
 
 from openprocurement.contracting.api.tests.base import (
     BaseWebTest as BaseBaseWebTest
@@ -15,6 +17,43 @@ test_common_contract_data = deepcopy(test_contract_data)
 test_common_contract_data['contractType'] = 'common'
 test_common_contract_data_wo_items = deepcopy(test_contract_data_wo_items)
 test_common_contract_data_wo_items['contractType'] = 'common'
+
+
+class DumpsTestAppwebtest(TestApp):
+    """ Class used for docs """
+    hostname = "api-sandbox.openprocurement.org"
+
+    def do_request(self, req, status=None, expect_errors=None):
+        req.headers.environ["HTTP_HOST"] = self.hostname
+        if hasattr(self, 'file_obj') and not self.file_obj.closed:
+            self.file_obj.write(req.as_bytes(True))
+            self.file_obj.write("\n")
+            if req.body:
+                try:
+                    self.file_obj.write(
+                            'DATA:\n' + json.dumps(json.loads(req.body), indent=2, ensure_ascii=False).encode('utf8'))
+                    self.file_obj.write("\n")
+                except:
+                    pass
+            self.file_obj.write("\n")
+        resp = super(DumpsTestAppwebtest, self).do_request(req, status=status, expect_errors=expect_errors)
+        if hasattr(self, 'file_obj') and not self.file_obj.closed:
+            headers = [(n.title(), v)
+                       for n, v in resp.headerlist
+                       if n.lower() != 'content-length']
+            headers.sort()
+            self.file_obj.write(str('Response: %s\n%s\n') % (
+                resp.status,
+                str('\n').join([str('%s: %s') % (n, v) for n, v in headers]),
+            ))
+
+            if resp.testbody:
+                try:
+                    self.file_obj.write(json.dumps(json.loads(resp.testbody), indent=2, ensure_ascii=False).encode('utf8'))
+                except:
+                    pass
+            self.file_obj.write("\n\n")
+        return resp
 
 
 class BaseWebTest(BaseBaseWebTest):
