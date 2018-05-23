c659fa9e4b32751c523b383a28790322ad9cb068
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-05-23 12:50:46 +0300

    WIP: change app_meta; fix some tests

--- openprocurement/contracting/common/tests/document.py
+++ openprocurement/contracting/common/tests/document.py
@@ -20,8 +20,11 @@ from openprocurement.contracting.common.tests.document_blanks import (
 
 
 class ContractDocumentResourceTest(BaseContractContentWebTest):
-    docservice = True
     initial_auth = ('Basic', ('broker', ''))
+
+    def setUp(self):
+        super(ContractDocumentResourceTest, self).setUp()
+        self.setUpDS()
 
     test_not_found = snitch(not_found)
     test_create_contract_document = snitch(create_contract_document)
